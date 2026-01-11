7751fbec50e2eee903ca9855b99a74888774c99e
m_igashi <@M_Igashi>
2026-01-11 23:37:55 +0100

    mp3rgain: update to v0.5.0
    - Add ReplayGain analysis (-r track gain, -a album gain)
    - Full mp3gain command-line compatibility

--- Formula/mp3rgain.rb
+++ Formula/mp3rgain.rb
@@ -1,9 +1,9 @@
 class Mp3rgain < Formula
   desc "Lossless MP3 volume adjustment - a modern mp3gain replacement written in Rust"
   homepage "https://github.com/M-Igashi/mp3rgain"
-  url "https://github.com/M-Igashi/mp3rgain/releases/download/v0.4.0/mp3rgain-v0.4.0-macos-universal.tar.gz"
-  sha256 "04d36f0aa5d5b2bd748a24fc2f50071192f033067e18f0275e6ebbe026652114"
-  version "0.4.0"
+  url "https://github.com/M-Igashi/mp3rgain/releases/download/v0.5.0/mp3rgain-v0.5.0-macos-universal.tar.gz"
+  sha256 "9ef2a1aafb721a319365f67c2b42123ce9587fb32fe8896d1f0c09cce6e2e892"
+  version "0.5.0"
   license "MIT"
 
   def install
