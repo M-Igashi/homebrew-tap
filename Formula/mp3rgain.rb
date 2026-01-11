3a530858c0b24d69e3b45d9c8b2c2d78365609f5
m_igashi <@M_Igashi>
2026-01-11 21:32:51 +0100

    mp3rgain: update to v0.4.0
    - APEv2 tag support for undo functionality (-u option)
    - Native implementation (no external dependencies)

--- Formula/mp3rgain.rb
+++ Formula/mp3rgain.rb
@@ -1,9 +1,9 @@
 class Mp3rgain < Formula
   desc "Lossless MP3 volume adjustment - a modern mp3gain replacement written in Rust"
   homepage "https://github.com/M-Igashi/mp3rgain"
-  url "https://github.com/M-Igashi/mp3rgain/releases/download/v0.3.0/mp3rgain-v0.3.0-macos-universal.tar.gz"
-  sha256 "ae0c892380be30409e77e3c06ea9ae0c3acf9bcbda73082058aa921e70b1dcc7"
-  version "0.3.0"
+  url "https://github.com/M-Igashi/mp3rgain/releases/download/v0.4.0/mp3rgain-v0.4.0-macos-universal.tar.gz"
+  sha256 "04d36f0aa5d5b2bd748a24fc2f50071192f033067e18f0275e6ebbe026652114"
+  version "0.4.0"
   license "MIT"
 
   def install
