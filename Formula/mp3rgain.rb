e4e9b590a11afe8b9a530c97251bf5c4408e2960
m_igashi <@M_Igashi>
2026-01-12 12:56:06 +0100

    mp3rgain: update to v0.9.0 (AAC/M4A support)

--- Formula/mp3rgain.rb
+++ Formula/mp3rgain.rb
@@ -1,9 +1,9 @@
 class Mp3rgain < Formula
-  desc "Lossless MP3 volume adjustment - a modern mp3gain replacement written in Rust"
+  desc "Lossless MP3/M4A volume adjustment - a modern mp3gain replacement written in Rust"
   homepage "https://github.com/M-Igashi/mp3rgain"
-  url "https://github.com/M-Igashi/mp3rgain/releases/download/v0.8.0/mp3rgain-v0.8.0-macos-universal.tar.gz"
-  sha256 "42e2903accd6803bf3a161d1b4f4ebddafc1dcbddb3571e27170b7315a630989"
-  version "0.8.0"
+  url "https://github.com/M-Igashi/mp3rgain/releases/download/v0.9.0/mp3rgain-v0.9.0-macos-universal.tar.gz"
+  sha256 "065c33d8c03466dbb95045363bbda13341385c405abea0f8cfb21f35d2d4309d"
+  version "0.9.0"
   license "MIT"
 
   def install
