a0c01e2c0dc5efe9fec619957ba8104c8f226e65
m_igashi <@M_Igashi>
2026-03-12 21:30:51 +0100

    Add mp3rgui Cask for macOS GUI distribution
    Install via: brew install --cask m-igashi/tap/mp3rgui

---
+++ Casks/mp3rgui.rb
@@ -0,0 +1,17 @@
+cask "mp3rgui" do
+  version "2.0.0"
+  sha256 "PLACEHOLDER"
+
+  url "https://github.com/M-Igashi/mp3rgain/releases/download/v#{version}/mp3rgui-v#{version}-macos-universal.dmg"
+  name "mp3rgui"
+  desc "GUI for lossless MP3/M4A volume adjustment - a modern mp3gain replacement"
+  homepage "https://github.com/M-Igashi/mp3rgain"
+
+  app "mp3rgui.app"
+
+  zap trash: [
+    "~/Library/Caches/com.github.m-igashi.mp3rgui",
+    "~/Library/Preferences/com.github.m-igashi.mp3rgui.plist",
+    "~/Library/Saved Application State/com.github.m-igashi.mp3rgui.savedState",
+  ]
+end
